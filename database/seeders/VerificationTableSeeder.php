0a3e820042938ff4e1751fab43747f2aa5b699a8
tanjilaAkhi <tanjilaakhi74@gmail.com>
2021-12-21 15:05:26 -0800

    nid verification

--- database/seeders/VerificationTableSeeder.php
+++ database/seeders/VerificationTableSeeder.php
@@ -14,12 +14,137 @@ class VerificationTableSeeder extends Seeder
      */
     public function run()
     {
+        //admin verification seeder
+        
         User::Create([
             'name'=>'User 1',
             'password'=>bcrypt('12345'),
             'email'=>'m@gmail.com'
 
         ]);
+
+//user NId verification seeder
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
+        User::Create([
+            'nid_no'=>bcrypt('1234567891012'),
+            'name'=>'',
+            'fname'=>'',
+            'mname'=>'',
+            'cell'=>'',
+            'email'=>'m@gmail.com',
+            'birthdate'=>'',
+            'address'=>''
+        ]);
+        
 
         
     }
